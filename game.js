76d0db713986b9138d2da2dce693997b6bd29a8a
黄君立 <link314@163.com>
2026-07-24 14:24:39 +0800

    Simplify fog mask boundary

--- game.js
+++ game.js
@@ -112,7 +112,7 @@ function renderMain() {
           ${button('关于', 'about', 'ghost')}
           ${save.expeditions || save.introSeen ? button('重置存档', 'reset-save', 'ghost danger') : ''}
         </div>
-        <p class="version">V1.3.8 · Fog Memory Contrast</p>
+        <p class="version">V1.3.9 · Simple Fog Mask</p>
       </div>
       <aside class="menu-note">
         <span>生存守则 01</span>
